3a7337bfcdb06a70b3ff44f2bdfb457da9673542
Joan Urgel <joanurgel55@gmail.com>
2023-07-28 07:01:49 +0800

    secondcommit

---
+++ database/seeders/RolesSeeder.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+
+class RolesSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        // Define the roles you want to insert
+        $roles = [
+            ['id' => 1, 'name' => 'admin'],
+            ['id' => 2, 'name' => 'supervisor'],
+            ['id' => 3, 'name' => 'trainee'],
+        ];
+
+        // Insert the roles into the role table
+        DB::table('roles')->insert($roles);
+    }
+}
